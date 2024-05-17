166f2406060d709d5230522d0ea449d63dca8564
turalvt <turalvt@code.edu.az>
2024-05-17 18:15:40 +0400

    feat: introduction

--- homework/script.js
+++ homework/script.js
@@ -1,1 +1,67 @@
-console.log(hello);
+////////////////////////TASK1//////////////////////
+console.log('TASK1');
+//1
+console.log(12*8);
+//2
+let x = (10/2);
+console.log(x);
+//3
+let  num1 = 17
+let  num2 = 20
+console.log(num1 + num2);
+//4
+let name = "Tural"
+let surname = "Tenzilzade"
+let year = "2007" 
+let info = `name:${name} surname:${surname} year:${year}`
+console.log(info);
+//5
+let m=(17 % 12)
+console.log(m);
+//6
+let cityname = "Baku"
+
+//////////////////////////TASK2////////////////////////////
+
+// let a = 1, b = 1;
+// let c = ++a;
+// let d = b++;
+
+// console.log(c); // ?=2
+// console.log(d); // ?=1
+// console.log(b); // ?=2
+//sebeb burda ++ bir vahid artirmaq demekdir burada 'a' 1 vahid artib sonra consolda isledilir
+//b ise isledildikden sonra ise 1 vahid artir buna gorede c= a+1=2 d=b b=1+1
+////////////////////////////////////TASK3///////////////////////////////////////
+
+// let a = 2;
+// let x = 1 + (a *= 2);
+// console.log(x); // 5
+//burada *= o demekdirki a(anin teze deyeri yeni4 )= a(anin kohne deyeri yeni 2)*2
+//belelikle a = 4 x= 4+1= 5
+/////////////////////////////TASK4////////////////////////////////////
+
+// console.log('test1', test); // ?= xeta test  deyeri yoxdur
+// {
+//     let test = "something"
+//     console.log('test2', test); // ?= test2 something
+// }
+// console.log('test3', test);  // ?= xeta test deyeri yoxdur
+
+
+// console.log('test1', test);  // ?= xeta test deyeri yoxdur
+// {
+//     var test = "something"
+//     console.log('test2', test); //?= test2 something
+// }
+// console.log('test3', test); //?= xeta test deyeri yoxudur
+
+//////////////////TASK5/////////////////
+// let name = "Ulfat"            // string
+// let surname = Zakirli         // string
+// let year = 2000               //number
+// year = "2000"                 // string
+// let city;                     // undefined
+// let qualification = null      // null
+// let obj = { name: 'ulfat' }   // object
+// let arr = ['a', 'b', 'c']     // array
