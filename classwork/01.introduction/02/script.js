de2860c98d5b9ad2a631cb82ba3a834176dbd461
turalvt <turalvt@code.edu.az>
2024-05-18 12:29:35 +0400

    feat: introduction

---
+++ classwork/01.introduction/02/script.js
@@ -0,0 +1,30 @@
+const user = {
+    firstName: "Tural" ,
+    lastname: "Tenzilzade" , 
+    age: "16" ,
+    info: {
+        address: "sulutepe" ,
+        city: "baku" ,
+         country: "aze" 
+    },
+    edu:  {
+        uni: "bdrnk" ,
+        point: "800" ,
+    }, 
+    work:  {
+        company: "OSCAR" ,
+        exper: "i dont need" 
+    }
+
+    
+    }     
+
+console.log(user.firstName);
+console.log(user.lastname);
+console.log(user.age);
+console.log(user.info);
+console.log(user.edu);
+console.log(user.work);
+console.log(user["first-name"]);
+user.lastname = "zakirli"
+console.log(user.lastname);
---
+++ classwork/01.introduction/kilo/script.js
@@ -0,0 +1,14 @@
+let kilo =Number(prompt("kilonuzu girin"))
+    let boy =Number(prompt("boyunuzu girin"))
+    let sonuc=kilo/(boy*boy)
+   
+    if(sonuc<18.5){
+    alert("ideal kilonun altindasiniz(zargana)")
+    }else if (18.5<=sonuc<=24.9){
+    alert ("ideal kilodasiniz")
+    }else if(25<=sonuc<=29.9){
+    alert("ideal kilonun uzerindesiniz")
+    }else if (30<=sonuc<=39.9){
+    alert("ideal kilonun cox ustundesininz(obez)")
+    }else if(sonuc>40)
+    alert("TEBRIKLER ARTIQ BIR INFLUCER OLDUNUZ")
